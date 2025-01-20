f53c97de6a679c53e95b0a6fda5c554ec296fd89
Ave Tillo <avegailtillo.pup@gmail.com>
2025-01-20 19:36:03 +0800

    User UI

---
+++ javascript/scripts.js
@@ -0,0 +1,21 @@
+document.getElementById('admin').addEventListener('click', function() {
+    window.location.href = 'admin.html';
+});
+
+document.getElementById('check').addEventListener('click', function() {
+    window.location.href = 'LRT1.html';
+});
+
+document.getElementById('simulate')?.addEventListener('click', function() {
+    alert('Simulate button clicked');
+});
+
+document.getElementById('details')?.addEventListener('click', function() {
+    alert('Details button clicked');
+});
+
+document.getElementById('facebook')?.addEventListener('click', function() {
+    window.location.href = 'https://www.facebook.com/OfficialLRTA';
+});
+
+
